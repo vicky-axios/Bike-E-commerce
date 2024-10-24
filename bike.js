5d6bf363ff4e39b205b95c24f7965fcb12f6ccc1
vicky <146782526+vicky-axios@users.noreply.github.com>
2024-10-24 13:30:16 +0530

    Add files via upload

---
+++ bike.js
@@ -0,0 +1,11 @@
+var sidenavbar = document.querySelector(".side-navbar")
+
+function showNavbar() {
+    
+    sidenavbar.style.left="0"
+}
+
+function closeNavbar()
+{
+     sidenavbar.style.left="-60%"
+}
